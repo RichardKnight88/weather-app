ebb0097a3f5cc5b2c1d669e599b0d951e37e995c
richardknight <rknightdesign@gmail.com>
2021-06-16 19:06:53 +0100

    accessing weather for London GB

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import axios from 'axios'
 
 function App() {
 
-  
+  // Spent time tracing a bug here. needed to add https:// - starting at api was working in insomnia
+  const getData = async () => {
+    try {
+      const { data } = await axios.get('http://api.openweathermap.org/data/2.5/find?q=London&units=metric&appid=dff3c764b6f0c9b9852bb2fb8f5bf85a')
+      console.log(data.list[0])
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-
+  getData()
 
   return (
 
