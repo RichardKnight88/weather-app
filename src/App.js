fcc38e9e91f9763fe5c6d0c76e65a417f7421dac
richardknight <rknightdesign@gmail.com>
2021-06-16 17:50:56 +0100

    bulma and axios added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
 function App() {
-  return <h1>Hello World</h1>
+
+  
+
+
+
+  return (
+
+    <h1>Hello World</h1>
+
+
+  )
 }
 
 export default App
