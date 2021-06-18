a9143383e5ba9306024bfe353bccd49e7f6e5fab
richardknight <rknightdesign@gmail.com>
2021-06-18 13:12:26 +0100

    Detailed Card styled

--- src/components/WeatherDetail.js
+++ src/components/WeatherDetail.js
@@ -5,7 +5,6 @@ import Rain from '../assets/Rain.png'
 import Snow from '../assets/Snow.png'
 import Storm from '../assets/Storm.png'
 import ClearDay from '../assets/Clear-day.png'
-import ClearNight from '../assets/Clear-night.png'
 
 
 import axios from 'axios'
@@ -23,6 +22,9 @@ const WeatherDetail = () => {
   const { id, weblinkId } = useParams()
 
   const cityId = parseInt(weblinkId)
+
+  let imageLink = ''
+  let weatherStatus = ''
 
 
   useEffect(() => {
@@ -76,33 +78,29 @@ const WeatherDetail = () => {
 
   console.log('FILTERED CITY', filteredCity)
 
-  // const logic = () => {
-  //   if (filteredCity) {
-  //     filteredCity = weather[0].main
-  //     if (weatherStatus === 'Clouds') {
-  //       imageLink = Clouds
-  //     }
-  //     if (weatherStatus === 'Rain') {
-  //       imageLink = Rain
-  //     }
-  //     if (weatherStatus === 'Clear') {
-  //       if (parseInt(time) < 16 && parseInt(time) > 6) {
-  //         imageLink = ClearDay
-  //       } else {
-  //         imageLink = ClearNight
-  //       }
-  //     }
-  //     if (weatherStatus === 'Snow') {
-  //       imageLink = Snow
-  //     }
-  //     if (weatherStatus === 'Storm') {
-  //       imageLink = Storm
-  //     }
-  //     return imageLink
-  //   }
-  // }
+  const logic = () => {
+    if (filteredCity) {
+      weatherStatus = filteredCity[0].weather[0].main
+      if (weatherStatus === 'Clouds' || weatherStatus === 'Mist') {
+        imageLink = Clouds
+      }
+      if (weatherStatus === 'Rain') {
+        imageLink = Rain
+      }
+      if (weatherStatus === 'Clear') {
+        imageLink = ClearDay
+      }
+      if (weatherStatus === 'Snow') {
+        imageLink = Snow
+      }
+      if (weatherStatus === 'Storm') {
+        imageLink = Storm
+      }
+      return imageLink
+    }
+  }
 
-  // logic()
+  logic()
 
 
   if (!filteredCity) return null
@@ -114,58 +112,68 @@ const WeatherDetail = () => {
     <>
       <section className={`section hero is-fullheight-with-navbar ${filteredCity[0].weather[0].main}`}>
 
-        {filteredCity.length < 1 ?
-          <h2 className=" has-text-centered errorBlock">
-            {hasError ? 'Oops, it looks like we do not have records for that city. Please click here to search again' : 'Loading...'}
-          </h2>
-          :
-          <>
+        <div className="container card detailedPage">
 
-            <div className="container detailedPage">
-              {/* <WeatherCard className="is-fullwidth" /> */}
-              <>
-                <section className="section detailHalf">
-                  <div className="container">
-                    <div className="columns">
-                      <div className="column is-fullwidth">
-                        <div className="card">
-                          <>
-                            <div className="card-header">
-                              <div className="card-header-title">Detailed Information</div>
-                            </div>
-                            <>
-                              <div className="card-content">
-                                <>
-                                  <div className="card-header-title is-2">Condtions</div>
-                                  <h2>{filteredCity[0].weather[0].description}</h2>
-                                </>
-                              </div>
-                              <div className="card-content">
-                                <>
-                                  <div className="card-header-title is-2">Temperature</div>
-                                  <h2>Feels Like: {filteredCity[0].main.feels_like}ºC</h2>
-                                  <h2>Min Temp: {filteredCity[0].main.temp_min}ºC</h2>
-                                  <h2>Max Temp: {filteredCity[0].main.temp_max}ºC</h2>
-                                  <h2>Humidity: {filteredCity[0].main.humidity}%</h2>
-                                </>
-                              </div>
-                              <div className="card-content">
-                                <>
-                                  <div className="card-header-title is-2">Wind Speed</div>
-                                  <h2>{filteredCity[0].wind.speed} m/s</h2>
-                                </>
-                              </div>
-                            </>
-                          </>
-                        </div>
-                      </div>
-                    </div>
+          {filteredCity.length < 1 ?
+            <h2 className=" has-text-centered errorBlock">
+              {hasError ? 'Oops, it looks like we do not have records for that city. Please search again' : 'Loading...'}
+            </h2>
+            :
+            <>
+
+              <div className="columns">
+
+                <div className="column">
+
+                  <div className="card-header-title">
+                    {filteredCity[0].name}, {filteredCity[0].sys.country}
                   </div>
-                </section>
-              </>
-            </div>
-          </>
-        }
+
+                  <div className="card-image">
+                    <figure className="image is-1by1">
+                      <img src={imageLink} alt={imageLink} />
+                    </figure>
+                  </div>
+
+                  <div className="card-content">
+                    <>
+                      <div className="card-header-title is-2">Condtions</div>
+                      <h2>{filteredCity[0].weather[0].description}</h2>
+                    </>
+                  </div>
+
+
+
+                  <>
+
+                    <div className="card-header-title is-1">Detailed Information
+                    </div>
+                    <>
+
+                      <div className="card-content">
+                        <>
+                          <div className="card-header-title is-2">Temperature</div>
+                          <h2>Feels Like: {filteredCity[0].main.feels_like}ºC</h2>
+                          <h2>Min Temp: {filteredCity[0].main.temp_min}ºC</h2>
+                          <h2>Max Temp: {filteredCity[0].main.temp_max}ºC</h2>
+                          <h2>Humidity: {filteredCity[0].main.humidity}%</h2>
+                        </>
+                      </div>
+                      <div className="card-content">
+                        <>
+                          <div className="card-header-title is-2">Wind Speed</div>
+                          <h2>{filteredCity[0].wind.speed} m/s</h2>
+                        </>
+                      </div>
+                    </>
+                  </>
+
+
+                </div>
+              </div>
+            </>
+          }
+        </div>
       </section>
     </>
 
