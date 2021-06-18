191ed6ea96a85fad3d3ecc75fd32ebe45ff32b6f
richardknight <rknightdesign@gmail.com>
2021-06-18 12:37:00 +0100

    favourites page added

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -5,8 +5,16 @@ const Favourites = () => {
 
   return (
 
-    <h1>FAVOURITES</h1>
-    
+    <section className="section hero is-fullheight-with-navbar searchPage">
+
+      <div className="container is-fullwidth is-flex is-align-items-center">
+
+        <div className="columns favouritesBlock is-half">
+          <h2>Welcome to the Favourites Tab</h2>
+          <h2>With more time we had grand plans to let you favourite a bunch of cities to display here.</h2>
+        </div>
+      </div>
+    </section>
   )
 
 }
